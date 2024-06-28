f2d7afdce7cddd277f64b97a8fdbcc17243bfdf7
nvim <prozskov@gmail.com>
2024-06-28 16:01:13 +0200

    Remove console.log

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { EvaluateResponse } from "./interfaces";
 
 export function useFHIRPathUI() {
-    console.log('env', process.env)
     const fhirPathServiceURL = process.env.REACT_APP_FHIRPATH_URL || 'http://localhost:5000';
     const evaluateURL = fhirPathServiceURL + '/evaluate'
     const [url, setUrl] = useState<string>('');
