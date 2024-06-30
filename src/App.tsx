d51ef0ee80116aad278043a5d984643a79118566
nvim <prozskov@gmail.com>
2024-06-30 10:36:36 +0200

    Update UI styles

--- src/App.tsx
+++ src/App.tsx
@@ -17,55 +17,50 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
+      <div className='infoButton'>
+        <button onClick={() => setInfoModalOpen(true)}><Info fontSize={24} /></button>
+      </div>
       {isLoading ? <Loader /> : null}
       <Modal show={infoModalOpen} onClose={() => setInfoModalOpen(false)}>
-        <>
+        <div className='infoContent'>
           <h1>Open source UI for the FHIRPath expression</h1>
           <p><a href="https://github.com/projkov/fhirpath-ui" target="_blank" rel="noreferrer">GitHub Repo</a></p>
           <p>Developed by <a href="https://github.com/projkov" target="_blank" rel="noreferrer">Pavel Rozhkov</a></p>
           <p><a href="https://github.com/beda-software/fhirpath-py" target="_blank" rel="noreferrer">FHIRPath</a> engine developed by <a href="https://beda.software" target="_blank" rel="noreferrer">Beda Software</a></p>
-        </>
-      </Modal>
-      <div className='mainContainer'>
-        <div className='sidebar'>
-          <img src={logo} alt="Logo" className='logo' />
-          <button onClick={() => setInfoModalOpen(true)}><Info fontSize={24} /></button>
         </div>
-        <div className='content'>
-          <div className='header'>
-            <div className='searchBlock'>
-              <input className="input" type="url" value={url} onChange={handleUrlChange} placeholder='You can paste the URL to get the FHIR Resource' />
-            </div>
-            <div className="buttonsBlock">
-              <button onClick={handleFetch} disabled={!isGetResourceActive}><FileArrowDown fontSize={24} /></button>
-              <button onClick={handleExecute} disabled={!isExecuteActive}><Play fontSize={24} /></button>
-              <button onClick={handleShare} disabled={!isShareActive}><ShareFat fontSize={24} /></button>
-            </div>
+      </Modal>
+      <div className='header'>
+        <img src={logo} alt="Logo" className='logo' />
+        <div className='searchBlock'>
+          <input className="input" type="url" value={url} onChange={handleUrlChange} placeholder='You can paste the URL to get the FHIR Resource' />
+        </div>
+        <div className="buttonsBlock">
+          <button onClick={handleFetch} disabled={!isGetResourceActive}><FileArrowDown fontSize={24} /></button>
+          <button onClick={handleExecute} disabled={!isExecuteActive}><Play fontSize={24} /></button>
+          <button onClick={handleShare} disabled={!isShareActive}><ShareFat fontSize={24} /></button>
+        </div>
+      </div>
+      <div className='editor'>
+        <Allotment defaultSizes={[550, 250]}>
+          <div className='editorWrapper'>
+            <Editor height="100vh" defaultLanguage="json" value={resource} onChange={(value) => setResource(value as string)} options={{ formatOnPaste: true, formatOnType: true }} />
           </div>
-          <div className='editor'>
-            <Allotment defaultSizes={[550, 250]}>
+          <div style={{ height: '100vh' }}>
+            <Allotment defaultSizes={[100, 300]} vertical>
               <div className='editorWrapper'>
-                <Editor height="100vh" defaultLanguage="json" value={resource} onChange={(value) => setResource(value as string)} options={{ formatOnPaste: true, formatOnType: true }} />
+                <Editor defaultLanguage="ruby" value={expression} onChange={(value) => setExpression(value as string)} options={{ formatOnPaste: true, formatOnType: true }} />
               </div>
-              <div style={{ height: '100vh' }}>
-                <Allotment defaultSizes={[100, 300]} vertical>
-                  <div className='editorWrapper'>
-                    <Editor defaultLanguage="ruby" value={expression} onChange={(value) => setExpression(value as string)} options={{ formatOnPaste: true, formatOnType: true }} />
-                  </div>
-                  <div className='editorWrapper'>
-                    <Editor defaultLanguage="json" value={result} options={{
-                      formatOnPaste: true,
-                      formatOnType: true,
-                      readOnly: true,
-                    }} />
-                  </div>
-                </Allotment>
+              <div className='editorWrapper'>
+                <Editor defaultLanguage="json" value={result} options={{
+                  formatOnPaste: true,
+                  formatOnType: true,
+                  readOnly: true,
+                }} />
               </div>
             </Allotment>
           </div>
-        </div>
+        </Allotment>
       </div>
-
     </div>
   );
 };
