599c30e0797ec768dbfaca84cd607ec2deadbbaf
nvim <prozskov@gmail.com>
2025-04-19 13:42:08 +0200

    Add import package Ref: https://github.com/projkov/fhirpath-ui/pull/25/files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,23 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true },
-  },
-  settings: {
-    react: { version: 'detect' },
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['react', 'react-hooks', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': 'warn',
-  },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+    },
+    settings: {
+        react: { version: 'detect' },
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'plugin:import/recommended',
+    ],
+    plugins: ['react', 'react-hooks', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'no-unused-vars': 'warn',
+    },
 };
